29efffdc6f913e32191df7257799add81c33fa79
emiliano <emiliano@alu.ufc.br>
2018-10-14 11:22:52 -0300

    adicionado a função do botão que desliza o menu lateral - adicionando classe

---
+++ atividade_5/script.js
@@ -0,0 +1,7 @@
+
+function addClass() {
+    var menu = document.querySelector(".menu-lateral");
+    var botao = document.querySelector(".botao-menu");
+    console.log(menu);
+    menu.classList.toggle("margem-zero");
+}
