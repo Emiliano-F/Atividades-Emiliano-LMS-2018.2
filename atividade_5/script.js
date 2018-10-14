19fdbd2e860a8dae82de50503f3d6473e24178e1
emiliano <emiliano@alu.ufc.br>
2018-10-14 11:31:19 -0300

    função correta corrigida - mudança de estilo por atributo de objeto HTMl

--- atividade_5/script.js
+++ atividade_5/script.js
@@ -1,7 +1,9 @@
 
+// funcao de adicionar classes ao clicar no botao
+
 function addClass() {
     var menu = document.querySelector(".menu-lateral");
     var botao = document.querySelector(".botao-menu");
-    console.log(menu);
-    menu.classList.toggle("margem-zero");
+    //menu.classList.toggle("margem-zero");
+    menu.style.marginLeft = "0px";
 }
