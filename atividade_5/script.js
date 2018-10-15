2312872f76fb80ac36ca62fde36e7e23e1e5ce59
emiliano <emiliano@alu.ufc.br>
2018-10-15 15:20:24 -0300

    finalizado as funcionalidades do site - precisando de pequenos ajustes finais

--- atividade_5/script.js
+++ atividade_5/script.js
@@ -3,23 +3,29 @@
 
 var on = false;
 
-
 function change() {
     var menu = document.querySelector(".menu-lateral");
     var botao = document.querySelector(".botao-menu");
     var titulo = document.querySelector(".titulo-site");
-    //menu.classList.toggle("margem-zero");
+    var fundo = document.querySelector(".fundo");
+    menu.classList.toggle("margem-zero");
     //menu.style.marginLeft = "0px"
-    //botao.style.marginLeft = "250px";
-    titulo.style.display = "none"
+    botao.style.marginLeft = "250px";
+    titulo.style.display = "none";
+    fundo.style.display = "block";
 
 
     //gambiarrazinha pra fazer o botao deslocar
     if(on){
-        //botao.style.marginLeft = "0px";
+        botao.style.marginLeft = "0px";
         titulo.style.display = "block";
+        fundo.style.display = "none";
         on = false;
         return;
     }
     on = true;
+}
+function height(x) {
+    var elem = document.querySelector("#t" + x);
+    elem.classList.toggle("altura");
 }
