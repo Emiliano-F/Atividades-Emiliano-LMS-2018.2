959606e3ab221d3006a3871b3ebd4f9b74d6757e
emiliano <emiliano@alu.ufc.br>
2018-10-15 13:11:49 -0300

    criado o corpo e estio do menu accordeon no corpo do site

--- atividade_5/script.js
+++ atividade_5/script.js
@@ -1,9 +1,25 @@
 
 // funcao de adicionar classes ao clicar no botao
 
-function addClass() {
+var on = false;
+
+
+function change() {
     var menu = document.querySelector(".menu-lateral");
     var botao = document.querySelector(".botao-menu");
+    var titulo = document.querySelector(".titulo-site");
     //menu.classList.toggle("margem-zero");
-    menu.style.marginLeft = "0px";
+    //menu.style.marginLeft = "0px"
+    //botao.style.marginLeft = "250px";
+    titulo.style.display = "none"
+
+
+    //gambiarrazinha pra fazer o botao deslocar
+    if(on){
+        //botao.style.marginLeft = "0px";
+        titulo.style.display = "block";
+        on = false;
+        return;
+    }
+    on = true;
 }
