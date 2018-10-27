f29ac26171e9cad69b75856c54d2c824def2ce5b
emiliano <emiliano@alu.ufc.br>
2018-10-27 20:58:13 -0300

    adicionado a interação hard coded entre os nomes dos grupos e o nome do grupo com chat ativado - início

--- trabalho_2-parte_1/script.js
+++ trabalho_2-parte_1/script.js
@@ -1,23 +1,51 @@
-/* var coluna = document.querySelector("#nome");
-coluna.addEventListener("click", add);
-
-function add() {
-    let n, img, nome;
-    n = document.createElement("div");
+var userId = 0;
+function addUser(nameUser) {
+    let card, img, nome;
+    card = document.createElement("div");
     img = document.createElement("img");
     nome = document.createElement("div");
+
+    card.id = userId;
+    userId++;
     
-    n.classList.add("contact-plane", "panel");
+    card.classList.add("contact-plane", "panel");
     img.classList.add("user-icon");
     nome.classList.add("user-name");
-    let txt = document.createTextNode("Nome criado");
+    let txt = document.createTextNode(nameUser);
     nome.appendChild(txt);
     
     img.src = "./img/icone.png";
 
-    n.appendChild(img);
-    n.appendChild(nome);
+    card.appendChild(img);
+    card.appendChild(nome);
 
-    document.querySelector(".users-column").appendChild(n);
+    document.querySelector("#coluna").appendChild(card); //seleção por id é mais segura - id unico no documento
+}
 
-} */
+let cont = 1
+for (let i = 0; i < 3; i++){
+    
+    addUser(`Grupo dinamicamente criado - ${cont}`);
+    cont++;
+}
+
+function letsChat(id) {
+    let chatUser = document.getElementById("chat-user");
+    let user = document.getElementById(id);
+    let userName = document.createTextNode(user.children[1].innerHTML);
+    let userImg = document.createElement("img");
+
+//    userImg.src = user.children[0].src;
+    chatUser.children[0].src = user.children[0].src;
+    chatUser.children[1].innerHTML = user.children[1].innerHTML;
+    
+}
+
+letsChat("1");
+
+/* 
+let user = document.getElementById("1");
+let userImg = document.createElement("img");
+userImg.src = user.src;
+
+console.log(userImg); */
