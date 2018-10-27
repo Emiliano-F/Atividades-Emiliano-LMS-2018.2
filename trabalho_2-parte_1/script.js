41808e6867df5688d5c4b2845dc716ae02c5d9a7
emiliano <emiliano@alu.ufc.br>
2018-10-27 19:47:00 -0300

    maiores adições realizadas - testes com a interação com javascript

---
+++ trabalho_2-parte_1/script.js
@@ -0,0 +1,23 @@
+/* var coluna = document.querySelector("#nome");
+coluna.addEventListener("click", add);
+
+function add() {
+    let n, img, nome;
+    n = document.createElement("div");
+    img = document.createElement("img");
+    nome = document.createElement("div");
+    
+    n.classList.add("contact-plane", "panel");
+    img.classList.add("user-icon");
+    nome.classList.add("user-name");
+    let txt = document.createTextNode("Nome criado");
+    nome.appendChild(txt);
+    
+    img.src = "./img/icone.png";
+
+    n.appendChild(img);
+    n.appendChild(nome);
+
+    document.querySelector(".users-column").appendChild(n);
+
+} */
