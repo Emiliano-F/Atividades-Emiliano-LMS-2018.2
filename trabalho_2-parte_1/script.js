f28e9eb86133420ba9d4695cd1eac43f0e9b9149
emiliano <emiliano@alu.ufc.br>
2018-11-03 20:07:03 -0300

    criando conexão com o servidor rest

--- trabalho_2-parte_1/script.js
+++ trabalho_2-parte_1/script.js
@@ -1,45 +1,74 @@
+let url = "http://rest.learncode.academy/api/emiliano/grupos";
+
+function getItems() {
+    let getServer = new XMLHttpRequest();
+    getServer.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            console.log("get");
+            let resBody = JSON.parse(this.responseText);
+            console.log(resBody);
+        }
+    }
+    getServer.open("GET", url, true);
+    getServer.send();    
+}
+
+
 //adicionar grupos dinamicamente
-var userId = 0;
-function addUser(nameUser) {
-    let card, img, nome;
-    card = document.createElement("div");
-    img = document.createElement("img");
-    nome = document.createElement("div");
+function addGroup(name) {
+    let column = document.getElementById('coluna');
+    let grupo = document.createElement('div');
+    let img = document.createElement('img');
+    let grupoNome = document.createElement("div");
 
-    card.id = userId;
-    userId++;
-    
-    card.classList.add("contact-plane", "panel");
-    img.classList.add("user-icon");
-    nome.classList.add("user-name");
-    let txt = document.createTextNode(nameUser);
-    nome.appendChild(txt);
-    
-    img.src = "./img/icone.png";
+    grupo.classList.add("contact-plane", "panel");
+    img.src = './img/icone.png';
+    img.classList.add('user-icon');
+    grupoNome.classList.add("user-name");
+    grupoNome.innerHTML = name;
 
-    card.appendChild(img);
-    card.appendChild(nome);
-    //lance de adicionar
-    card.addEventListener("click", function () {
-        letsChat(card.id);
-    })
-
-    document.querySelector("#coluna").appendChild(card); //seleção por id é mais segura - id unico no documento
+    grupo.appendChild(img);
+    grupo.appendChild(grupoNome);
+    column.appendChild(grupo);
 }
-
-//chamar a função que adiciona 3 vezes
-let cont = 1
-for (let i = 0; i < 3; i++){
-    
-    addUser(`Grupo dinamicamente criado - ${cont}`);
-    cont++;
-}
+let grupos = [
+    {
+        grupo: 'grupo da familia',
+        menssagens: [
+            {
+                usuario: "joao",
+                texto: "Oi td bem?"
+            },
+            {
+                usuario: "victor",
+                texto: "To de buenass"
+            },
+            {
+                usuario: "joao",
+                texto: "que bom"
+            }
+        ]
+    },
+    {
+        grupo: "Paquera de DD",
+        menssagens: [
+            {
+                usuario: "ismael",
+                texto: "Oi bbs S2!"
+            },
+            {
+                usuario: "igo",
+                texto:"Oi gatinho"
+            }
+        ]
+    }
+];
 
 //mudar o chat
-function letsChat(id) {
+/* function letsChat(id) {
     let chatUser = document.getElementById("chat-user");
     let user = document.getElementById(id);
 
     chatUser.children[0].src = user.children[0].src;
     chatUser.children[1].innerHTML = user.children[1].innerHTML;    
-}
+} */
